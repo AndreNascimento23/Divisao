4978febfef2a59b4dcb96637deab7f4865e82cf7
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-01 21:10:54 -0300

    Divisor

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+   double numerador, denominador, resultado;
+
+Console.Write("Digite o Númerador: ");
+numerador = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o Denominador: ");
+denominador = Convert.ToInt32(Console.ReadLine());
+
+
+if (denominador == 0)
+{
+ Console.WriteLine("Nao pode dividir por Zero"); 
+}
+
+else 
+ {
+   resultado = numerador / denominador;
+
+  Console.WriteLine($"{numerador} dividido por {denominador} e {resultado}");
+ }
